877ccc9a41cb5b8439e926c212d899c0582baf90
silviacasstillo <silcasrui@alum.es>
2025-10-05 12:29:19 +0200

    consulta

--- consulta4.sql
+++ consulta4.sql
@@ -1,13 +1,14 @@
-DROP TABLE IF EXISTS usuarios;
 DROP TABLE IF EXISTS transacciones;
 
-CREATE TABLE  usuarios(
-       
-       usuarioId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       nombre VARCHAR(45) NOT NULL,
-       apellidos VARCHAR(45) NOT NULL,
-       usuarioName VARCHAR(45) NOT NULL UNIQUE,
-       contraseña VARCHAR(256) NOT NULL  
+DROP TABLE IF EXISTS usuarios;
+
+CREATE TABLE usuarios(
+    usuarioId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(45) NOT NULL,
+    apellidos VARCHAR(45) NOT NULL,
+    usuarioName VARCHAR(45) NOT NULL UNIQUE,
+    contraseña VARCHAR(256) NOT NULL,
+    numero_cuenta VARCHAR(20) UNIQUE
 );
 
 CREATE TABLE transacciones(
@@ -17,13 +18,9 @@ CREATE TABLE transacciones(
         usuario_destino INT NOT NULL,
         cantidad DECIMAL(7,2) NOT NULL,
         fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        nonce VARCHAR(128),
+        mac VARCHAR(128),
         FOREIGN KEY (usuario_origen) REFERENCES usuarios(usuarioId),
         FOREIGN KEY (usuario_destino) REFERENCES usuarios(usuarioId)
-
 );
-
-
-INSERT INTO usuarios(usuarioId,nombre,apellidos,usuarioName,contraseña) VALUES
-         (1,'amara','innocent','rolerAmari','7f911f82c99bedf7706b049138310ffc523f385840357f0345694c0a413cb212'),
-         (2,'silvia','castillo','silcasrubi','7e27a8af00e2c6918334632848a3569f38fdedb133bcf09f3432143e86ebbf13'),
-         (3,'victor','ramos','vicyToler3','20765013cbc8d4bd0eefa211233bd30e295468463d3d3dcdb20e3958c1d83f2f.');
+usuarios
